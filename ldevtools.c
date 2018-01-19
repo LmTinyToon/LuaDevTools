be3df39f83998302c3f4391b1badcf0f5b5aeb38
Alex <cool.ipatovo@yandex.ru>
2018-01-19 19:28:59 +0300

    Full printing of hash table of strings was added

--- ldevtools.c
+++ ldevtools.c
@@ -354,17 +354,18 @@ void (ldv_dump_ldv_heap_at_mem)(const void* ptr)
 
 void (ldv_dump_hash_strtable)(lua_State* L)
 {
-	stringtable hash_string_table = G(L)->strt;
-	ldv_log("======= HASH STRING TABLE DUMP (nuse: %i) (size: %i) ==============\n", hash_string_table.nuse, hash_string_table.size);
-	for (int i = 0; i < hash_string_table.nuse; ++i)
+	stringtable* hash_string_table = &G(L)->strt;
+	ldv_log("======= HASH STRING TABLE DUMP (nuse: %i) (size: %i) ==============\n", hash_string_table->nuse, hash_string_table->size);
+	for (int i = 0; i < hash_string_table->nuse; ++i)
 	{
-		TString* str = hash_string_table.hash[i];
-		if (str == NULL)
+		ldv_log("=========	List %i of elements	========\n", i);
+		TString **p = &hash_string_table->hash[i];
+		while (*p != NULL) 
 		{
-			ldv_log("NULL \n");
-			continue;
+			ldv_dump_short_string(L, *p);
+			p = &(*p)->u.hnext;
 		}
-		ldv_dump_short_string(L, str);
+		ldv_log("=======================================\n");
 	}
 	ldv_log("==========================================================\n");
 }
